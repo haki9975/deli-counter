9f7d9ff26b7f88f9697735189c6a7a277e1e0199
haki9975 <elias.robert.hakim@gmail.com>
2021-02-05 20:17:10 -0500

    commit tree d998581f79ef1fe730865706bb62aee963972395

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require 'pry'
+
+def line(array)
+   if array.length > 0
+     new_line = array.collect.with_index(1) {|name, i| "#{i}. #{name}"}
+        puts "The line is currently: " + new_line.join(" ")     
+    else
+        puts "The line is currently empty."
+   end
+end
+
+def take_a_number(array, name)
+    if array.length >= 0
+      array.push(name) 
+       puts "Welcome, #{name}. You are number #{array.size} in line."
+    else
+        puts "The line is empty."
+    end
+end
+
+def now_serving(array)
+    if array.length > 0
+        puts "Currently serving #{array.first}."
+        array.shift
+    else 
+        puts "There is nobody waiting to be served!"
+    end
+end
